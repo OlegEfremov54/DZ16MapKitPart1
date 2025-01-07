0bc3c3365ed49d7c3c7b3b35a95302022c93262d
OlegEfremov54 <ole_f@mail.ru>
2025-01-07 22:14:45 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.dz16mapkitpart1"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.dz16mapkitpart1"
+        minSdk = 33
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.foundation.android)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //implementation (libs.maps.mobile)
+    implementation ("com.yandex.android:maps.mobile:4.3.1-full")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$rootProject.coroutines")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$rootProject.coroutines")
+
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
+}
---
+++ app/src/main/java/com/example/dz16mapkitpart1/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.example.dz16mapkitpart1
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Canvas
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dz16mapkitpart1.databinding.ActivityMainBinding
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.MapObjectCollection
+import com.yandex.mapkit.map.PlacemarkMapObject
+import com.yandex.mapkit.map.TextStyle
+import com.yandex.mapkit.search.Response
+import com.yandex.mapkit.search.Session
+import com.yandex.runtime.image.ImageProvider
+import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.search.SearchFactory
+import com.yandex.mapkit.search.SearchManagerType
+import com.yandex.mapkit.search.SearchOptions
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var location = Point(53.2122, 50.1438)
+    private val zoomValue = 16.5f
+
+    private lateinit var mapObjectCollection: MapObjectCollection
+    private lateinit var placemarkMapObject: PlacemarkMapObject
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setApiKey(savedInstanceState)
+        MapKitFactory.initialize(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val lat = intent.getDoubleExtra("lat", location.latitude)
+        val lon = intent.getDoubleExtra("lon", location.longitude)
+        location = Point(lat, lon)
+        binding.mapView.moveToLocation(location, zoomValue)
+        setMarkerToLocation(location)
+
+
+    }
+    private fun setApiKey(savedInstanceState: Bundle?) {
+        val haveApiKey: Boolean = savedInstanceState?.getBoolean("haveApiKey") ?:false
+        if (!haveApiKey)
+            MapKitFactory.setApiKey("@string.MAPKIT_API_KEY")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("haveApiKey",true)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        binding.mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    private fun MapView.moveToLocation(location: Point, zoom: Float) {
+        this.mapWindow.map.move(
+            CameraPosition(
+                location,
+                zoom,
+                0f,
+                0f
+            ),
+            Animation(Animation.Type.SMOOTH, 3f),
+            null
+        )
+        val searchOptions = SearchOptions()
+        val searchManager = SearchFactory.getInstance()
+            .createSearchManager(SearchManagerType.ONLINE)
+        searchManager.submit(
+            location,
+            zoom.toInt(),
+            searchOptions,
+            object : Session.SearchListener {
+                override fun onSearchResponse(p0: Response) {
+                    placemarkMapObject.setText(
+                        p0.collection.children.first().obj?.name ?: "Нет данных"
+                    )
+                }
+
+                override fun onSearchError(p0: com.yandex.runtime.Error) {
+                    Log.d("aaa", "onSearchError: search ERROR")
+                }
+
+
+
+            }
+        )
+    }
+
+    private fun setMarkerToLocation(location: Point) {
+        val marker = createBitmapFromVector(R.drawable.baseline_place_24)
+        mapObjectCollection = binding.mapView.mapWindow.map.mapObjects
+        placemarkMapObject = mapObjectCollection.addPlacemark(
+            location,
+            ImageProvider.fromBitmap(marker)
+        )
+        placemarkMapObject.opacity = 0.5f
+        }
+    }
+
+    private fun createBitmapFromVector(art: Int): Bitmap? {
+        val drawable = ContextCompat.getDrawable(this, art) ?: return null
+        val bitmap = Bitmap.createBitmap(
+            drawable.intrinsicWidth,
+            drawable.minimumHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = android.graphics.Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return bitmap
+    }
+
